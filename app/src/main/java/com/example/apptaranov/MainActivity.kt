2313f02085c381b262db9be7fceef2651316c94c
Илья Таранов <ilja-taranov@yandex.ru>
2020-11-23 15:20:53 +0300

    Add movie details screen

--- app/src/main/java/com/example/apptaranov/MainActivity.kt
+++ app/src/main/java/com/example/apptaranov/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.apptaranov
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        movie_details_btn.setOnClickListener { showMovieDetails() }
+    }
+
+    private fun showMovieDetails() {
+        startActivity(Intent(this, MovieDetailsActivity::class.java))
     }
 }
