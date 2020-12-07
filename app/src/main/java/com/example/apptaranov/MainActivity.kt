6a99f9945681b90b34a9c04c96247fe4dab68e09
Илья Таранов <ilja-taranov@yandex.ru>
2020-12-07 11:01:05 +0300

    Add movie list screen

--- app/src/main/java/com/example/apptaranov/MainActivity.kt
+++ app/src/main/java/com/example/apptaranov/MainActivity.kt
@@ -1,19 +1,18 @@
 package com.example.apptaranov
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        movie_details_btn.setOnClickListener { showMovieDetails() }
-    }
-
-    private fun showMovieDetails() {
-        startActivity(Intent(this, MovieDetailsActivity::class.java))
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, FragmentMoviesList.newInstance())
+                .addToBackStack(FragmentMoviesList::class.java.simpleName)
+                .commit()
+        }
     }
 }
